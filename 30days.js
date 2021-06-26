a4c8567522cb0b619a8a8fec5b67627516863323
Ana Ceprnja <aceprnja@iun.edu>
2021-06-26 10:08:35 -0600

    hello world in c

--- 30days.js
+++ 30days.js
@@ -5,4 +5,13 @@ function processData(inputString) {
     console.log(inputString);
 }
 
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
 
+process.stdin.on("end", function () {
+    processData(_input);
+});
